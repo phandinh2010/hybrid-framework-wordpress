764ebb4bc7406e293111df900897203c8f54c295
dinhpt5 <dinhpt5@VID-DINHPT5-L.Vingroup.local>
2021-04-09 17:36:52 +0700

    update java basic

--- javaCore/javaBasic/Topic_04_Dynamic_Locator.java
+++ javaCore/javaBasic/Topic_04_Dynamic_Locator.java
@@ -1,20 +1,49 @@
 package javaBasic;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import commons.BasePage;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class Topic_04_Dynamic_Locator {
+public class Topic_04_Dynamic_Locator extends BasePage {
 	
+	//1 - Click vào từng đường link ứng với mỗi locator
 	public static final String ABOUT_US="//a[text()='About us']";
 	public static final String PRODUCT_LINK="//a[text()='New product']";
 	
-	public static final String DYNAMIC_LINK="//a[text()='New product']";
+	//2 - chỉ cần định nghĩa 1 locator động
+	public static final String DYNAMIC_FOOTER_LINK="//a[text()='%s']";
+	
+	//3 - 1 locator có 2 tham số dynamic
+	public static final String DYNAMIC_LINK="//a[text()='%s']";
+	
+	// 4 - Có 3 tham số dynamic
+	
+	
 	
 	public static void main(String[] args) {
+		clickToElement(DYNAMIC_FOOTER_LINK, "About us");
+		clickToElement(DYNAMIC_FOOTER_LINK, "New product");		
+		
+		clickToElement(DYNAMIC_LINK, "heard", "Register" );
+		clickToElement(DYNAMIC_LINK, "heard", "Login" );
+		
+		clickToElement(DYNAMIC_LINK, "footer", "About us" );
 		
 	}
-
+		
+	// 1- 1 tham số động
 	
+	public static void clickToElement(String locator, String pageName) {		
+		System.out.println(locator);
+	}
+	
+	//2 tham số động
+	public static void clickToElement(String locator, String panel, String pageName) {		
+		System.out.println(locator);
+	}
+	
+	//n tham số động
+	// Tham số này phải đứng ở cuối cùng
+	//Phải cùng kiểu dữ liệu khi truyền vào sử dụng
+	public static void clickToElement(String locator, String... values) {		
+		System.out.println(String.format(locator, (Object[]) values));
+	}
 }
--- javaCore/javaBasic/Topic_05_String_Fomat.java
+++ javaCore/javaBasic/Topic_05_String_Fomat.java
@@ -1,15 +1,19 @@
 package javaBasic;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Topic_05_String_Fomat {
 	
 		
 	public static void main(String[] args) {
+		String fullName = "Xin mời anh chị: %s";
 		
+		String[] khachMoi = {"Nguyễn Văn A", "Nguyễn B", "Nguyễn C"};
+		
+		for (String khach : khachMoi) {
+		System.out.println(String.format(fullName, khach));
+			
+		}
 		
 	}
 
